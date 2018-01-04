1935211ce633a7744a4d26565e955591954c2ce2
yanxiaojun <664891740@qq.com>
2018-01-04 09:17:51 +0800

    0104 0900

---
+++ src/com/iotek/ssm/dao/DepartmentDao.java
@@ -0,0 +1,19 @@
+package com.iotek.ssm.dao;
+
+import java.util.List;
+
+import com.iotek.ssm.entity.Department;
+
+public interface DepartmentDao {
+	
+	public int addDept(Department dept);
+	
+	public int delDept(int id);
+	
+	public int updateDept(String name);
+	
+	public Department queryById(int id);
+	
+	public List<Department> queryAllDept();
+
+}
---
+++ src/com/iotek/ssm/dao/PositionDao.java
@@ -0,0 +1,19 @@
+package com.iotek.ssm.dao;
+
+import java.util.List;
+
+import com.iotek.ssm.entity.Position;
+
+public interface PositionDao {
+	
+	public int addPost(Position post);
+	
+	public int delPost(int id);
+	
+	public int updatePost(String name);
+	
+	public Position queryById(int id);
+	
+	public List<Position> queryByDeptId();
+
+}
